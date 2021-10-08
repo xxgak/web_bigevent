a28a6124614f96fb22bdba637317e571838e9b28
xxgak <1934245878@qq.com>
2021-10-09 01:31:03 +0800

    完成了登陆和注册的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,21 +1,31 @@
 $(function() {
   // 点击“去注册账号”的链接
+/*   $('#link_reg').on('click', function() {
+    $('.login-box').hide()
+    $('.reg-box').show()
+  }) */
   $('#link_reg').on('click', function() {
     $('.login-box').hide()
     $('.reg-box').show()
   })
 
+
   // 点击“去登录”的链接
+  /* $('#link_login').on('click', function() {
+    $('.login-box').show()
+    $('.reg-box').hide()
+  }) */
   $('#link_login').on('click', function() {
     $('.login-box').show()
     $('.reg-box').hide()
   })
 
+
   // 从 layui 中获取 form 对象
   var form = layui.form
   var layer = layui.layer
   // 通过 form.verify() 函数自定义校验规则
-  form.verify({
+ /*  form.verify({
     // 自定义了一个叫做 pwd 校验规则
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
     // 校验两次密码是否一致的规则
@@ -29,10 +39,20 @@ $(function() {
         return '两次密码不一致！'
       }
     }
+  }) */
+  form.verify({
+    pwd: [/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'],
+    repwd: function(value) {
+      var pwd = $('.reg-box [name=password]').val()
+      if(value !== pwd) {
+        return '两次密码不一致！'
+      }
+    }
   })
 
+
   // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
+/*   $('#form_reg').on('submit', function(e) {
     // 1. 阻止默认的提交行为
     e.preventDefault()
     // 2. 发起Ajax的POST请求
@@ -48,9 +68,26 @@ $(function() {
       // 模拟人的点击行为
       $('#link_login').click()
     })
-  })
+  }) */
+ $('#form_reg').on('submit', function(e) {
+    e.preventDefault()
+    var data = {
+      username: $('#form_reg [name=username]').val(),
+      password: $('#form_reg [name=password]').val()
+    }
+    $.post('/api/reguser', data, function(res) {
+      if(res.status !== 0) {
+        return layer.msg(res.message)
+      }
+      layer.msg('注册成功，请您登陆')
+    })
+    $('#link_login').click()
+ })
 
-  // 监听登录表单的提交事件
+
+
+
+/*   // 监听登录表单的提交事件
   $('#form_login').submit(function(e) {
     // 阻止默认提交行为
     e.preventDefault()
@@ -70,5 +107,25 @@ $(function() {
         location.href = '/index.html'
       }
     })
-  })
+  }) */
+  $('#form_login').submit(function(e) {
+    e.preventDefault()
+    $.ajax({
+      url: '/api/login',
+      method: 'post',
+      data: $(this).serialize(),
+      success: function(res) {
+        if(res.status !== 0) {
+          // return console.log('登陆失败！')
+          return layer.msg('登陆失败！')
+        }
+        // console.log('登陆成功')
+        layer.msg('登陆成功！')
+        localStorage.setItem('token', res.token)
+        location.href = '/index.html'
+      }
+    })
+  }) 
+
+
 })
